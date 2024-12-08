9238d71fe7ad749ef0de9c4bd44c1fada7850d03
Sami Abu Touq <samiabutouq050@gmail.com>
2024-12-08 13:58:14 +0300

    Fix routing for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
   }, []);
 
   return (
-    <Router>
+    <Router basename="/sami">
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
@@ -38,9 +38,8 @@ function App() {
           <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
           <Route path="/resume" element={<Resume />} />
-          <Route path="*" element={<Navigate to="/"/>} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
-       
       </div>
     </Router>
   );
