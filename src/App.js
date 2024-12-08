950466188f7cfa32ce3a6b30ea2aa7ddaf0c7655
Sami Abu Touq <samiabutouq050@gmail.com>
2024-12-08 14:10:00 +0300

    Fix

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ function App() {
         <Navbar />
         <ScrollToTop />
         <Routes>
+          
           <Route path="/" element={<Home />} />
           <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
